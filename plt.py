a36cd40f968403bf6a48689fb41b265b97754fb3
Daniel <danielbarriosrocha@gmail.com>
2024-07-15 15:23:58 -0500

    Graficador

---
+++ plt.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,10,100)
+y = np.sin(x)
+plt.plot(x,y,"g")
+plt.title("F(x) = Sen(x)")
+plt.xlabel("X")
+plt.ylabel("Sen (x)")
+plt.grid()
+plt.xlim(0,10)
+plt.ylim(-3,3)
+plt.savefig("Sen(x).pdf")
+plt.show()
